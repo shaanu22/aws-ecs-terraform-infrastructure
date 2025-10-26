7227996f59eedfdc7b54ffd77a9410d0946eb2ee
shaanu22 <mails2samtaiwo@gmail.com>
2025-10-25 21:24:13 -0500

    Initial commit - empty main branch

--- terraform/environments/dev/backend.tf
+++
@@ -1,7 +0,0 @@
-# This file will be populated after running the backend-setup
-
-terraform {
-  backend "s3" {
-    # No values here - configured at init time
-  }
-}
--- terraform/modules/monitoring/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cpu_alarm_arn" {
-  description = "ARN of the CPU alarm"
-  value       = aws_cloudwatch_metric_alarm.ecs_cpu_high.arn
-}
-
-output "memory_alarm_arn" {
-  description = "ARN of the memory alarm"
-  value       = aws_cloudwatch_metric_alarm.ecs_memory_high.arn
-}
